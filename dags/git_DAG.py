508b058bae400dc416c94775fc31bedbf091c3b6
Donald Hobern <dhobern@gmail.com>
2023-09-16 16:04:45 +1000

    Pull updates from Github

---
+++ dags/git_DAG.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+import datetime
+
+dag = DAG(
+    dag_id="git_updates",
+    start_date=datetime.datetime(2023, 8, 10, tzinfo=local_tz),
+    schedule_interval="0 0 * * *",
+    default_args={"email": ["dhobern@gmail.com"], "email_on_failure": True},
+    catchup=False,
+)
+
+zip = BashOperator(
+    task_id="pull",
+    bash_command="cd /home/dhobern/gli_tools && gh fetch && gh pull && cp dags/*.py /home/dhobern/airflow/dags",
+    dag=dag,
+)
