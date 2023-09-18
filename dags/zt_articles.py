8801cee96c85f3441120a9cb930c9a03a9088c3f
Donald Hobern <dhobern@gmail.com>
2023-09-18 13:25:44 +1000

    Log progress

--- dags/zt_articles.py
+++ dags/zt_articles.py
@@ -3,6 +3,8 @@ from lxml import etree
 import csv
 import os
 import lep_config
+import logging
+
 
 def extract_zt_articles():
     folder = lep_config.folder
@@ -11,26 +13,53 @@ def extract_zt_articles():
     file_articles = os.path.join(folder, "zootaxa.csv")
 
     if os.path.exists(file_new):
-        with open(file_articles, "w", newline='', encoding='utf8') as f:
+        with open(file_articles, "w", newline="", encoding="utf8") as f:
             writer = csv.writer(f, delimiter=",")
-            writer.writerow(["journal", "url", "title", "authors", "issue", "pages", "image"])
-            with open(file_new, newline='', encoding='utf8') as f:
+            writer.writerow(
+                ["journal", "url", "title", "authors", "issue", "pages", "image"]
+            )
+            with open(file_new, newline="", encoding="utf8") as f:
                 reader = csv.reader(f, delimiter=",")
                 next(reader)
                 for issue in reader:
+                    logging.info(f"Processing issue: {issue}")
                     page = urlopen(issue[2])
-                    html = page.read().decode("utf-8").replace("<strong>", "").replace("</strong>", "").replace("<em>", "!@em@!").replace("</em>", "!@/em@!").replace("<span>Page range:</span>", "")
+                    html = (
+                        page.read()
+                        .decode("utf-8")
+                        .replace("<strong>", "")
+                        .replace("</strong>", "")
+                        .replace("<em>", "!@em@!")
+                        .replace("</em>", "!@/em@!")
+                        .replace("<span>Page range:</span>", "")
+                    )
                     tree = etree.HTML(html)
                     articles = tree.xpath("//div[@class='obj_article_summary']")
                     for article in articles:
-                        title = article.xpath(".//h2[@class='title']//a")[0].text.strip().replace("!@", "<").replace("@!", ">").replace("  ", " ")
+                        title = (
+                            article.xpath(".//h2[@class='title']//a")[0]
+                            .text.strip()
+                            .replace("!@", "<")
+                            .replace("@!", ">")
+                            .replace("  ", " ")
+                        )
                         if "lepidoptera" in title.lower():
-                            url = article.xpath(".//h2[@class='title']//a")[0].attrib["href"].strip()
-                            authors = article.xpath(".//div[@class='authors']")[0].text.strip()
-                            pages = article.xpath(".//div[@class='pages']")[2].text.strip()     
+                            url = (
+                                article.xpath(".//h2[@class='title']//a")[0]
+                                .attrib["href"]
+                                .strip()
+                            )
+                            authors = article.xpath(".//div[@class='authors']")[
+                                0
+                            ].text.strip()
+                            pages = article.xpath(".//div[@class='pages']")[
+                                2
+                            ].text.strip()
                             images = article.xpath(".//img")
-                            if images is not None and len(images) > 0: 
+                            if images is not None and len(images) > 0:
                                 image = images[0].attrib["src"].strip()
                             else:
                                 image = ""
-                            writer.writerow(["Zootaxa", url, title, authors, issue[1], pages, image])
+                            writer.writerow(
+                                ["Zootaxa", url, title, authors, issue[1], pages, image]
+                            )
