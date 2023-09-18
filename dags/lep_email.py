b8716fa801857cfbf09c771c739ff4f32c009358
Donald Hobern <dhobern@gmail.com>
2023-09-18 13:48:36 +1000

    More logging

--- dags/lep_email.py
+++ dags/lep_email.py
@@ -4,23 +4,21 @@ from email.mime.multipart import MIMEMultipart
 from airflow.hooks.base_hook import BaseHook
 import os
 import lep_config
+import logging
 
-def send_email(credentials,
-               email_recipient,
-               email_subject,
-               email_location):
-    
+
+def send_email(credentials, email_recipient, email_subject, email_location):
     email_sender = credentials.login
 
     msg = MIMEMultipart()
-    msg['From'] = email_sender
-    msg['To'] = email_recipient
-    msg['Subject'] = email_subject
+    msg["From"] = email_sender
+    msg["To"] = email_recipient
+    msg["Subject"] = email_subject
 
-    with open(email_location, newline='', encoding='utf8') as f:
-        body = ''.join(f.readlines())
+    with open(email_location, newline="", encoding="utf8") as f:
+        body = "".join(f.readlines())
 
-    msg.attach(MIMEText(body, 'html'))
+    msg.attach(MIMEText(body, "html"))
 
     try:
         server = smtplib.SMTP(credentials.host, credentials.port)
@@ -29,12 +27,13 @@ def send_email(credentials,
         server.login(email_sender, credentials.password)
         text = msg.as_string()
         server.sendmail(email_sender, email_recipient, text)
-        print('email sent')
+        print("email sent")
         server.quit()
     except:
         print("SMTP server connection error")
         return False
     return True
+
 
 def lepidoptera_email():
     credentials = BaseHook.get_connection("smtp_default")
@@ -52,4 +51,5 @@ def lepidoptera_email():
         subject = f"Latest Lepidoptera articles ({tag})"
         if not send_email(credentials, "dhobern@gmail.com", subject, filename):
             exit(1)
+        logging.info(f"Removing {filename}")
         os.remove(filename)
